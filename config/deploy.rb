362c24c830d24c0b46b6d873c8934c48d0c682e3
Robs-v82 <roberto.valladarespiedras@gmail.com>
2020-08-11 12:04:59 -0500

    example

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "lantiamaster"
-set :repo_url, "git@example.com:Robs-v8/lantiamaster2.git"
+set :repo_url, "git@github.com:Robs-v8/lantiamaster2.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
