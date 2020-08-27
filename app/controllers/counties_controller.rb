d444196c85de508e11c48aa93db7bf63a7b47b8b
Robs-v82 <roberto.valladarespiedras@gmail.com>
2020-08-27 17:36:04 -0500

    second attempt order

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -2,7 +2,11 @@ class CountiesController < ApplicationController
 
 	def getCounties
 	    targetState = getCounties_params[:state_id].to_i
-	    targetCounties = State.find(targetState).counties.sort
+	   	targetCounties = State.find(targetState).counties
+	   	undefinedCounty = targetCounties.where(:name=>"Sin definir").last
+	   	targetCounties = targetCounties.where.not(:name=>"Sin definir")
+	   	targetCounties = targetCounties.sort_by{|county|county.name}
+	    targetCounties << undefinedCounty
 	    render json: {counties: targetCounties}		
 	end
 
