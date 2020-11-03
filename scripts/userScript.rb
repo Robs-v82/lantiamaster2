fab5825a58b34bb643069df54c6958d87677f8e8
Roberto VP <roberto.valladarespiedras@gmail.com>
2020-11-03 16:38:26 -0600

    font9897

--- scripts/userScript.rb
+++ scripts/userScript.rb
@@ -1,4 +1,5 @@
 myMembers = [
+	[{:firstname=>"Roberto",:lastname1=>"Valladares",:lastname2=>"Piedras"},{:mail=>"roberto@primeraraiz.com",:password=>"Paloma001",:password_confirmation=>"Paloma001"}],
 	[{:firstname=>"Jorge",:lastname1=>"Zendejas",:lastname2=>"Reyes"},{:mail=>"jorgezen@gmail.com",:password=>"Zacatecas27",:password_confirmation=>"Zacatecas27"}],
 	[{:firstname=>"Eduardo",:lastname1=>"Guerrero",:lastname2=>"Gutiérrez"},{:mail=>"eggmexico@gmail.com",:password=>"Guerrero08",:password_confirmation=>"Guerrero08"}],
 	[{:firstname=>"Eunises",:lastname1=>"Rosillo",:lastname2=>"Ortiz"},{:mail=>"eunisesrosillo@gmail.com",:password=>"Tequisquiapan58",:password_confirmation=>"Tequisquiapan58"}],
