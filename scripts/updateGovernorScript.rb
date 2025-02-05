6abe02c8022b2042ca485f0e943ff783af04c53f
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-02-05 12:26:49 -0600

    gobernoasuyiuyay

--- scripts/updateGovernorScript.rb
+++ scripts/updateGovernorScript.rb
@@ -1,81 +1,77 @@
-# rawData = "02,Marina del Pilar,Ávila,Olmeda,gobernador02.jpg
-# 	03,Víctor,Castro,Cosio,gobernador03.jpg
-# 	04,Layda,Sansores,San Román,gobernador04.jpg
-# 	08,María Eugenia,Campos,Galván,gobernador08.jpg
-# 	06,Indira,Vizcaíno,Silva,gobernador06.jpg
-# 	12,Evelyn,Salgado,Pineda,gobernador12.jpg
-# 	16,Alfredo,Ramírez,Bedolla,gobernador16.jpg
-# 	18,Miguel Ángel,Navarro,Quintero,gobernador18.jpg
-# 	22,Mauricio,Kuri,González,gobernador22.jpg
-# 	24,Ricardo,Gallardo,Cardona,gobernador24.jpg
-# 	25,Rubén,Rocha,Moya,gobernador25.jpg
-# 	26,Alfonso,Durazo,Montaño,gobernador26.jpg
-# 	29,Lorena,Cuéllar,Cisneros,gobernador29.jpg"
-
-rawData = "01,María Teresa,Jiménez,Esquivel,gobernador01.jpg
-10,Esteban,Villegas,Villareal,gobernador10.jpg
-28,Américo,Villarreal,Anaya,gobernador28.jpg
-09,Martí,Batres,Guadarrama,gobernador09.jpg
-21,Sergio,Céspedes,Peregrina,gobernador21.jpg
-23,Mara,Lezama,Espinosa,gobernador23.jpg
-20,Salomón,Jara,Cruz,gobernador20.jpg
-07,Rutilio,Escandón,Cadenas,gobernador07.jpg
-27,Carlos Manuel,Merino,Campos,gobernador27.jpg
-32,David,Monreal,Ávila,gobernador32.jpg
-13,Julio,Menchaca,Salazar,gobernador13.jpg"
-
-if Role.where(:name=>"Gobernador").empty?
-	Role.create(:name=>"Gobernador")	
-end
-governorKey = Role.where(:name=>"Gobernador").last.id
+rawData = "14,Pablo,Lemus,Navarro,gobernador14.jpg
+30,Rocío,Nahle,García,gobernador30.jpg
+31,Joaquín,Díaz,Mena,gobernador31.jpg
+11,Libia Dennise,García,Muñoz Ledo,gobernador11.jpg
+17,Margarita,González,Saravia,gobernador17.jpg
+07,Eduardo,Ramírez,Aguilar,gobernador07.jpg"
 
 governorArr = []
 rawData.each_line{|l| line = l.split(","); governorArr.push(line)}
 governorArr.each{|x|x.each{|y|y.strip!}}
 
-State.all.each{|state|
-	myName = "Gubernatura de "+state.name
-	myCode = state.code+"000"
-	myCounty = County.where(:full_code=>myCode).last.id
-	if Organization.where(:name => myName).empty?
-		Organization.create(:name => myName, :league => "CONAGO", :county_id => myCounty)
-	end
-	government = Division.where(:scian3=>931).last
-	Organization.last.divisions << government
-	}
+governorArr.each{|x|
+	state = State.where(:code=>x[0]).last
+	g = Organization.where(:name=>"Gubernatura de "+state.name).last.members.last
+	g.update(:firstname => x[1],
+		:lastname1 =>  x[2],
+		:lastname2 => x[3])
+	g.avatar.purge
+	downloaded_image = URI.open(Rails.root / 'app' / 'assets' / 'images' / x[4])
+	governor.avatar.attach(io: downloaded_image, filename: x[4])
+}
+
+print "Ready :-)"
+
+# if Role.where(:name=>"Gobernador").empty?
+# 	Role.create(:name=>"Gobernador")	
+# end
+# governorKey = Role.where(:name=>"Gobernador").last.id
+
+
+
+# State.all.each{|state|
+# 	myName = "Gubernatura de "+state.name
+# 	myCode = state.code+"000"
+# 	myCounty = County.where(:full_code=>myCode).last.id
+# 	if Organization.where(:name => myName).empty?
+# 		Organization.create(:name => myName, :league => "CONAGO", :county_id => myCounty)
+# 	end
+# 	government = Division.where(:scian3=>931).last
+# 	Organization.last.divisions << government
+# 	}
 
 # UPATE GOVERNORS' INFORMATION
-State.all.each{|state|
-	myName = "Gubernatura de "+state.name
-	organization_id = Organization.where(:name => myName).last.id
-	stateMembers = Organization.where(:name => myName).last.members
-	governor = stateMembers.where(:role_id=>governorKey).last
-	governorArr.each{|x|
-		filename = "gobernador"+x[1]+".jpg"
-		downloaded_image = URI.open(Rails.root / 'app' / 'assets' / 'images' / x[4])
-		print filename
-		if x[0] == state.code
-			if governor == nil	
-				Member.create(
-					:firstname => x[1],
-					:lastname1 =>  x[2],
-					:lastname2 => x[3],
-					:organization_id => organization_id,
-					:role_id => governorKey
-				)
-				member.last.avatar.attach(io: downloaded_image, filename: filename)
-			else
-				governor.update(
-					:firstname => x[1],
-					:lastname1 =>  x[2],
-					:lastname2 => x[3],
-				)
-				governor.avatar.purge
-				governor.avatar.attach(io: downloaded_image, filename: filename)
-			end
-		end
-	}		
+# State.all.each{|state|
+# 	myName = "Gubernatura de "+state.name
+# 	organization_id = Organization.where(:name => myName).last.id
+# 	stateMembers = Organization.where(:name => myName).last.members
+# 	governor = stateMembers.where(:role_id=>governorKey).last
+# 	governorArr.each{|x|
+# 		filename = "gobernador"+x[1]+".jpg"
+# 		downloaded_image = URI.open(Rails.root / 'app' / 'assets' / 'images' / x[4])
+# 		print filename
+# 		if x[0] == state.code
+# 			if governor == nil	
+# 				Member.create(
+# 					:firstname => x[1],
+# 					:lastname1 =>  x[2],
+# 					:lastname2 => x[3],
+# 					:organization_id => organization_id,
+# 					:role_id => governorKey
+# 				)
+# 				member.last.avatar.attach(io: downloaded_image, filename: filename)
+# 			else
+# 				governor.update(
+# 					:firstname => x[1],
+# 					:lastname1 =>  x[2],
+# 					:lastname2 => x[3],
+# 				)
+# 				governor.avatar.purge
+# 				governor.avatar.attach(io: downloaded_image, filename: filename)
+# 			end
+# 		end
+# 	}		
 	
-}
+# }
 
 # testUpdate
