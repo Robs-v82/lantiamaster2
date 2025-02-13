c7c32ca5737cbe8c3e8827be5ba38a43d9f7732b
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-02-13 11:35:48 -0600

    hjjhg7777fffsdsds

--- scripts/iconDownloadScript.rb
+++ scripts/iconDownloadScript.rb
@@ -1,7 +1,7 @@
 years = ["2022","2023","2024"]
 quarters = ["_Q1","_Q2","_Q3","_Q4"]
-gobArr = []
-years.each{|y| quarters.each{|q| thisArr =[y+q]; myQuarter = Quarter.where(:name=>y+q).last.id; myCookie = Cookie.where(:category=>"icon", :quarter_id=>myQuarter).last.data; myCookie.each{|c| thisArr.push(c["gob"])}; gobArr.push(thisArr)}}
+visArr = []
+years.each{|y| quarters.each{|q| thisArr =[y+q]; myQuarter = Quarter.where(:name=>y+q).last.id; myCookie = Cookie.where(:category=>"icon", :quarter_id=>myQuarter).last.data; myCookie.each{|c| thisArr.push(c["vis"])}; visArr.push(thisArr)}}
 
 
 
