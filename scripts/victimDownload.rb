92cf42cc8b0c8476d04885e3d6b905ff0e670cdd
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-03-04 09:58:25 -0600

    mbmnbmnnmb5

--- scripts/victimDownload.rb
+++ scripts/victimDownload.rb
@@ -1,5 +1,6 @@
 require 'pp'
 myQuarters = Quarter.joins(:year).where(years: { name: ["2022", "2023", "2024"] })
+myQuarters = myQuarters.sort_by(&:name)
 
 typeOfPlaces = [
 	{:string=>"Vía pública", :typeArr=>["Vía pública (calle, avenida, banqueta, carretera)","Transporte privado (automóvil, motocicleta, bicileta)"], :color=>"#3EBF3E"},
@@ -8,29 +9,58 @@ typeOfPlaces = [
 	{:string=>"Transporte de pasajeros", :typeArr=>["Transporte público colectivo (autobús, metro, tren)","Transporte público privado (taxi, UBER, mototaxi)"], :color=>"#EF974E"}			
 ]
 quarterNames = myQuarters.pluck(:name)
-mainArr = [["Estado", "Tipo de lugar"] + quarterNames]
 myStates = State.all.sort_by{|s| s.code}
-myStates.each{|state|
-	localVictims = state.victims
-	localKillings = state.killings
-	typeOfPlaces.each{|type|
-		stateArr = [state.name]
-		stateArr.push(type[:string])
-		typeKillings = localKillings.where(:type_of_place=>type[:typeArr])
+
+booleans = [
+	{:string=>"massacres", :killings=>Killing.where("killed_count > ?", 3).where(:mass_grave=>nil)},
+	{:string=>"mass_graves", :killings=>Killing.where(:mass_grave=>true)},
+	{:string=>"shootings_authorities", :killings=>Killing.where(:any_shooting=>true)}					
+]
+
+booleans.each{|boolean|
+	mainArr = [["Estado","Tipo de víctima"] + quarterNames]
+	myStates.each{|state|
+		stateArr = [state.name,boolean[:string]]
+		localVictims = state.victims
+		localKillings = boolean[:killings].merge(state.killings)	
 		myQuarters.each{|q|
 			periodKillings = q.killings
-			targetKillings = typeKillings.merge(periodKillings)
+			targetKillings = localKillings.merge(periodKillings)
 			counter = targetKillings.joins(:victims).count
 			stateArr.push(counter)
 		}
 		mainArr.push(stateArr)
+		mainArr.push([state.name,"Total"])
+		myQuarters.each{|q|
+			periodVictims = q.victims
+			targetVictims = localVictims.merge(periodVictims)
+			mainArr[-1].push(targetVictims.count)
+		}
 	}
-	mainArr.push([state.name,"Total"])
-	myQuarters.each{|q|
-		periodVictims = q.victims
-		targetVictims = localVictims.merge(periodVictims)
-		mainArr[-1].push(targetVictims.count)
-	}
+	pp mainArr
 }
 
-pp mainArr
+
+# mainArr = [["Estado", "Tipo de lugar"] + quarterNames]
+# myStates.each{|state|
+# 	localVictims = state.victims
+# 	localKillings = state.killings
+# 	typeOfPlaces.each{|type|
+# 		stateArr = [state.name]
+# 		stateArr.push(type[:string])
+# 		typeKillings = localKillings.where(:type_of_place=>type[:typeArr])
+# 		myQuarters.each{|q|
+# 			periodKillings = q.killings
+# 			targetKillings = typeKillings.merge(periodKillings)
+# 			counter = targetKillings.joins(:victims).count
+# 			stateArr.push(counter)
+# 		}
+# 		mainArr.push(stateArr)
+# 	}
+# 	mainArr.push([state.name,"Total"])
+# 	myQuarters.each{|q|
+# 		periodVictims = q.victims
+# 		targetVictims = localVictims.merge(periodVictims)
+# 		mainArr[-1].push(targetVictims.count)
+# 	}
+# }
