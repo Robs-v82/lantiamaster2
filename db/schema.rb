4ac9dd2843b111abf1daa31216a42f810a54433d
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-04-22 02:34:06 -0300

    nameScrtjkjh89897

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_10_145106) do
+ActiveRecord::Schema.define(version: 2025_04_22_050932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -390,6 +390,13 @@ ActiveRecord::Schema.define(version: 2025_04_10_145106) do
     t.integer "quarter_id", null: false
     t.datetime "first_day"
     t.index ["quarter_id"], name: "index_months_on_quarter_id"
+  end
+
+  create_table "names", force: :cascade do |t|
+    t.string "word"
+    t.integer "freq"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "organization_towns", force: :cascade do |t|
