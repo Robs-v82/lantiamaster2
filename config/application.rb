a67430de4fc3744c4fd534c1cf7f56076a970f01
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-04-24 12:45:32 -0300

    timemexico

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Lantiamaster
 	 config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
 	 config.i18n.default_locale = :es
 
+   config.time_zone = 'Mexico City'
+  config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
