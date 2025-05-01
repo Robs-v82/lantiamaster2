4e81550bd148def997d1b35199efa38f285cfd96
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-05-01 17:16:09 -0300

    kjhjkhiuiuiuyaaa

--- scripts/pdfScript.rb
+++ scripts/pdfScript.rb
@@ -1,15 +1,20 @@
-require 'grover'
+require 'wicked_pdf'
 
 myHits = Hit.left_outer_joins(:pdf_attachment).where(active_storage_attachments: { id: nil }).limit(10)
-myHits = myHits.where.not(:link=>nil)
+myHits = myHits.where.not(link: nil)
+
+pdf_generator = WickedPdf.new
+
 myHits.each do |hit|
   next unless hit.link.present? && hit.link.start_with?('http')
 
   begin
     puts "🌀 Generando PDF para: #{hit.link}"
 
-    pdf = Grover.new(hit.link).to_pdf
+    # Renderiza el PDF desde la URL
+    pdf = pdf_generator.pdf_from_url(hit.link)
 
+    # Adjunta el PDF al modelo
     filename = "hit_#{hit.id}.pdf"
     io = StringIO.new(pdf)
     hit.pdf.attach(io: io, filename: filename, content_type: 'application/pdf')
