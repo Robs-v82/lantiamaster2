8ebbe10b1f38556a6c3aeba5a48b50f80bdf320f
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-06-03 18:49:03 -0600

    jhgjklkjhdaskjiu6titiuygiuygjg

--- scripts/stateGuards.rb
+++ scripts/stateGuards.rb
@@ -39,10 +39,9 @@ CSV.foreach(file_path, headers: true) do |row|
     member.update(
       start_date: start_date,
       end_date: end_date,
-      criminal_link: member.organization,
       organization: gn,
-      involved: true,
-      role: delegado_role
+      role: delegado_role,
+      involved: false
     )
     if member.end_date?
       member.update(:end_date=>end_date)
@@ -61,7 +60,6 @@ CSV.foreach(file_path, headers: true) do |row|
       start_date: start_date,
       end_date: end_date,
       organization: gn,
-      criminal_link: criminal_org,
       involved: false,
       role: delegado_role
     )
@@ -70,21 +68,27 @@ CSV.foreach(file_path, headers: true) do |row|
     end
   end
 
-  state = State.find_by(code: state_code)
-  full_code = state.capital.full_code + "0000"
-  town = Town.find_by(full_code: full_code)
+  if CRIMINAL_MAP.include? state_name
+    member.update(
+      criminal_link: criminal_org 
+    )
+    state = State.find_by(code: state_code)
+    full_code = state.capital.full_code + "0000"
+    town = Town.find_by(full_code: full_code)
 
-  # Asignación de legacy_id único con fecha y random string
-  unique_legacy = "#{start_date.strftime('%Y%m%d')}-#{SecureRandom.hex(4)}"
+    # Asignación de legacy_id único con fecha y random string
+    unique_legacy = "#{start_date.strftime('%Y%m%d')}-#{SecureRandom.hex(4)}"
 
-  hit = Hit.create(
-    date: start_date,
-    user: user,
-    title: "Nombramiento",
-    town: town,
-    legacy_id: unique_legacy
-  )
-  member.hits << hit unless member.hits.include?(hit)
+    hit = Hit.create(
+      date: start_date,
+      user: user,
+      title: "Nombramiento",
+      town: town,
+      legacy_id: unique_legacy
+    )
+    member.hits << hit unless member.hits.include?(hit)
+  end
+
 end
 
 puts "Carga completa de coordinadores estatales de la Guardia Nacional."
