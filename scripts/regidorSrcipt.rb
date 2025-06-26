fff1e7fd2992a41bce85f08e5cb80cb92bb99257
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-06-25 20:52:47 -0600

    readykjkjhkhjuy76878766ggg2

--- scripts/regidorSrcipt.rb
+++ scripts/regidorSrcipt.rb
@@ -1,9 +1,11 @@
 r = Role.find_by_name("Regidor")
 Member.where(:role=>r).each{|member|
-	org = member.hits.last.town.county.organizations.first
-	link = member.organization
-	member.update(
-		:organization=>org,
-		:criminal_link=>link
-		)
+	if member.hits.any?
+		org = member.hits.last.town.county.organizations.first
+		link = member.organization
+		member.update(
+			:organization=>org,
+			:criminal_link=>link
+			)
+	end
 }
