4f27aca8acd71b362a7071e13327a612a4460e89
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2025-09-05 13:35:32 -0600

    allhasheschanged

--- config/initializers/security_headers.rb
+++ config/initializers/security_headers.rb
@@ -1,14 +1,14 @@
-# CSP de compatibilidad: SOLO en producción
 if Rails.env.production?
   Rails.application.config.action_dispatch.default_headers.merge!({
-    'Content-Security-Policy' =>
+    'Content-Security-Policy' => 
       "default-src 'self'; " \
       "img-src 'self' data: https:; " \
       "font-src 'self' https://fonts.gstatic.com data:; " \
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " \
-      "script-src 'self' 'unsafe-inline' https://code.highcharts.com https://cdnjs.cloudflare.com; " \
+      "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; " \
       "connect-src 'self' ws: wss:; " \
       "object-src 'none'; base-uri 'self'; frame-ancestors 'none'; " \
       "form-action 'self'; upgrade-insecure-requests"
   })
 end
+
