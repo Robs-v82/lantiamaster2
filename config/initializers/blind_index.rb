0c72b3d4dd28c811041b665677e33ba185aecaad
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2026-02-03 18:13:58 -0600

    Encrypt Query fields (Lockbox + BlindIndex)8

--- config/initializers/blind_index.rb
+++ config/initializers/blind_index.rb
@@ -1,14 +1,15 @@
-# config/initializers/blind_index.rb
 key = Rails.application.credentials.dig(:blind_index, :master_key)
 
-# Assets precompile loads the app but doesn't need BlindIndex.
-# Avoid failing deploys during assets:precompile.
+# Detect rake tasks early (before Rake.application.top_level_tasks is ready)
+is_assets_task = ARGV.any? { |a| a.start_with?("assets:") }
+
 if key.present?
   BlindIndex.master_key = key
-elsif defined?(Rake) && Rake.application&.top_level_tasks&.any? { |t| t.start_with?("assets:") }
-  # skip
+elsif is_assets_task
+  # skip during assets tasks (e.g., assets:precompile)
 else
   raise "BlindIndex key missing in credentials"
 end
+
 
 
--- config/initializers/lockbox.rb
+++ config/initializers/lockbox.rb
@@ -1,11 +1,12 @@
-# config/initializers/lockbox.rb
 key = Rails.application.credentials.dig(:lockbox, :master_key)
+is_assets_task = ARGV.any? { |a| a.start_with?("assets:") }
 
 if key.present?
   Lockbox.master_key = key
-elsif defined?(Rake) && Rake.application&.top_level_tasks&.any? { |t| t.start_with?("assets:") }
-  # skip during assets precompile
+elsif is_assets_task
+  # skip during assets tasks
 else
   raise "Lockbox key missing in credentials"
 end
+
 
