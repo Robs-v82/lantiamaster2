df3421c9373979e60e0f02c39e3e76d63c0e0f18
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2026-02-03 18:06:31 -0600

    Encrypt Query fields (Lockbox + BlindIndex)7

--- config/initializers/blind_index.rb
+++ config/initializers/blind_index.rb
@@ -1,4 +1,14 @@
+# config/initializers/blind_index.rb
 key = Rails.application.credentials.dig(:blind_index, :master_key)
-raise "BlindIndex key missing in credentials" if key.blank?
-BlindIndex.master_key = key
+
+# Assets precompile loads the app but doesn't need BlindIndex.
+# Avoid failing deploys during assets:precompile.
+if key.present?
+  BlindIndex.master_key = key
+elsif defined?(Rake) && Rake.application&.top_level_tasks&.any? { |t| t.start_with?("assets:") }
+  # skip
+else
+  raise "BlindIndex key missing in credentials"
+end
+
 
--- config/initializers/lockbox.rb
+++ config/initializers/lockbox.rb
@@ -1,3 +1,11 @@
+# config/initializers/lockbox.rb
 key = Rails.application.credentials.dig(:lockbox, :master_key)
-raise "Lockbox key missing in credentials" if key.blank?
-Lockbox.master_key = key
+
+if key.present?
+  Lockbox.master_key = key
+elsif defined?(Rake) && Rake.application&.top_level_tasks&.any? { |t| t.start_with?("assets:") }
+  # skip during assets precompile
+else
+  raise "Lockbox key missing in credentials"
+end
+
