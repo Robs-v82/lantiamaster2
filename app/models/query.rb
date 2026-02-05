f022b2bc7d5b343d9279a71c370c58766488cdf8
Roberto Valladares <roberto.valladarespiedras@gmail.com>
2026-02-04 20:30:03 -0600

    fix problem with queries

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,15 +2,6 @@ class Query < ApplicationRecord
   belongs_to :user
   belongs_to :member, optional: true
   belongs_to :organization, optional: true
-
-  has_encrypted :firstname
-  has_encrypted :lastname1
-  has_encrypted :lastname2
-  has_encrypted :query_label
-  has_encrypted :outcome
-
-  blind_index :query_label, key: Rails.application.credentials.dig(:blind_index, :master_key)
-
   serialize :outcome, Array
-  scope :successful, -> { where(success: true) }
+  scope :successful, -> { where(success: true) } 
 end
